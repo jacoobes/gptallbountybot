6f7655d8d5419421e455d3cbeab4ab514a27bb47
jacoobes <jacobnguyend@gmail.com>
2023-04-24 13:42:39 -0500

    first commit

---
+++ src/index.ts
@@ -0,0 +1,50 @@
+import { Client, GatewayIntentBits } from 'discord.js';
+import {
+	Dependencies,
+	Sern,
+	single,
+	Singleton,
+	DefaultLogging,
+} from '@sern/handler';
+
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMembers,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent, //Make sure this is enabled for text commands!
+	],
+});
+
+//With typescript, you can customize / augment your typings.
+interface MyDependencies extends Dependencies {
+	'@sern/client': Singleton<Client>;
+	'@sern/logger': Singleton<DefaultLogging>;
+}
+/**
+ * Where all of your dependencies are composed.
+ * '@sern/client' is usually your Discord Client.
+ * View documentation for pluggable dependencies
+ * Configure your dependency root to your liking.
+ * It follows the npm package iti https://itijs.org/.
+ * Use this function to access all of your dependencies.
+ * This is used for external event modules as well
+ */
+export const useContainer = Sern.makeDependencies<MyDependencies>({
+	build: (root) =>
+		root
+			.add({ '@sern/client': single(() => client) })
+			.upsert({ '@sern/logger': single(() => new DefaultLogging()) }), //using upsert because it replaces the default provided
+});
+
+//View docs for all options
+Sern.init({
+	defaultPrefix: '!', // removing defaultPrefix will shut down text commands
+	commands: 'dist/commands',
+	// events: 'dist/events' (optional),
+	containerConfig: {
+		get: useContainer,
+	},
+});
+
+client.login();
